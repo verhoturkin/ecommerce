5d2919359e3c1aace61fdc142c452d06a8738629
Alexander Verkhoturkin <verhoturkin@gmail.com>
2022-10-09 23:17:19 +0500

    add CORS fix

--- src/main/java/ru/verkhoturkin/ecommerce/dao/ProductCategoryRepository.java
+++ src/main/java/ru/verkhoturkin/ecommerce/dao/ProductCategoryRepository.java
@@ -2,8 +2,10 @@ package ru.verkhoturkin.ecommerce.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import ru.verkhoturkin.ecommerce.entity.ProductCategory;
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
--- src/main/java/ru/verkhoturkin/ecommerce/dao/ProductRepository.java
+++ src/main/java/ru/verkhoturkin/ecommerce/dao/ProductRepository.java
@@ -1,7 +1,9 @@
 package ru.verkhoturkin.ecommerce.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import ru.verkhoturkin.ecommerce.entity.Product;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
