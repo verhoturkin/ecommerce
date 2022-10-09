85500f3e19be8f973a963534ea6f0240c1d902ac
Alexander Verkhoturkin <verhoturkin@gmail.com>
2022-10-09 21:53:27 +0500

    add repositories

---
+++ src/main/java/ru/verkhoturkin/ecommerce/dao/ProductCategoryRepository.java
@@ -0,0 +1,9 @@
+package ru.verkhoturkin.ecommerce.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import ru.verkhoturkin.ecommerce.entity.ProductCategory;
+
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+}
