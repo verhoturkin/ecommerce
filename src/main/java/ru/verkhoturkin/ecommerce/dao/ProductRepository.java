b66e9c74db40fed7221ff39754204e65aa6e34ca
Alexander Verkhoturkin <verhoturkin@gmail.com>
2023-01-08 21:42:26 +0500

    add keyword search

--- src/main/java/ru/verkhoturkin/ecommerce/dao/ProductRepository.java
+++ src/main/java/ru/verkhoturkin/ecommerce/dao/ProductRepository.java
@@ -12,4 +12,6 @@ import ru.verkhoturkin.ecommerce.entity.Product;
 @SuppressWarnings("unused")
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
+
+    Page<Product> findByNameContaining(@Param("name") String name, Pageable pageable);
 }
